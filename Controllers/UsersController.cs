27af70a343033f01c5d9e844699ef4d684967b33
FelipeGallego <gallegofelipe29@gmail.com>
2024-04-03 21:15:39 -0500

    Create and delete

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Proyecto.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Proyecto.Models;
 
 namespace Proyecto.Controllers
 {
@@ -22,6 +23,34 @@ namespace Proyecto.Controllers
         public async Task<IActionResult> Details(int? id)
         {
             return View(await _context.Users.FirstOrDefaultAsync(m => m.Id == id));
+        }
+
+        public async Task<IActionResult>Delete(int? id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        //se genera otra action para hacer todo el proceso de eliminado
+        // public async Task<IActionResult> Delete2(int? id)
+        // {
+        //     var user =  await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+        //     _context.Users.Remove(user);
+        //     await _context.SaveChangesAsync();
+        //     return RedirectToAction("Index");
+        // }        
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateUser(User u)
+        {
+            _context.Users.Add(u);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
